9255e90dffcceb7b1613b7a0979eafd78c784b43
SangHak <k3hppk@gmail.com>
2020-07-03 10:44:53 +0900

    Task 4 is done

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+"use strict"
+
+// Display Element
+const display = document.getElementById('display');
+
+// A variable to store the display value
+let displayValue = '';
+
+// All number elements
+const numbers = document.getElementsByClassName("number");
+
+// Basic Operators
+const operators = document.getElementsByClassName("operator");
+
+// equal element
+const equal = document.getElementById('equal');
+
+
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -25,3 +44,46 @@ function operate(operator, num1, num2) {
     return divide(num1, num2);
   }
 }
+
+// Function that will populate the display.
+// It's passed a value, which can be an operator, a number,
+// or anything else
+function populateDisplay(value) {
+
+  // If there's no input yet, replace 0 with a new input
+  if(display.innerHTML === '0') {
+    display.innerHTML = value;
+  } else {
+    // If there's already an input, just add the value
+    display.innerHTML = display.innerHTML + value;
+    updateDisplayValue(display.innerHTML);
+  }
+}
+
+// Update Display Value
+function updateDisplayValue(display) {
+  displayValue = display;
+}
+
+// Adding event listeners to all number elements
+// If number button is clicked, it will populate the display.
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener('click', (event) => {
+    let number = numbers[i].innerHTML;
+    populateDisplay(number);
+  })
+}
+
+// If operator is clicked
+for (let i = 0; i < operators.length; i++) {
+  operators[i].addEventListener('click', (event) => {    
+  })
+}
+
+// User presses an equal key
+equal.addEventListener('click', (event) => {
+
+})
+
+
+
